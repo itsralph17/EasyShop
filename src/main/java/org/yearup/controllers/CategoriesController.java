e872b11a6c8a0507277a227eb3108012ca541e1f
itsralph17 <rapraku@appdev.yearup.org>
2023-06-27 09:30:58 -0400

    controller completed

---
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -0,0 +1,93 @@
+package org.yearup.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.yearup.data.CategoryDao;
+import org.yearup.data.ProductDao;
+import org.yearup.models.Category;
+import org.yearup.models.Product;
+
+import java.util.List;
+
+// add the annotations to make this a REST controller
+// add the annotation to make this controller the endpoint for the following url
+    // http://localhost:8080/categories
+// add annotation to allow cross site origin requests
+@RestController
+@RequestMapping("categories")
+@CrossOrigin
+public class CategoriesController
+{
+    private CategoryDao categoryDao;
+    private ProductDao productDao;
+
+    // create an Autowired controller to inject the categoryDao and ProductDao
+
+    // add the appropriate annotation for a get action
+@Autowired
+    public CategoriesController(CategoryDao categoryDao, ProductDao productDao) {
+        this.categoryDao = categoryDao;
+        this.productDao = productDao;
+    }
+
+@GetMapping
+    public List<Category> getAll(@RequestParam(name = "categoryId", required = false) Integer categoryId,
+                                 @RequestParam(name = "name", required = false) String name,
+                                 @RequestParam(name = "description", required = false) String  description)
+    {
+        // find and return all categories
+        return categoryDao.getAllCategories(categoryId, name,description);
+
+
+    }
+
+    // add the appropriate annotation for a get action
+    @GetMapping("/{id}")
+    public Category getById(@PathVariable int categoryId)
+    {
+        // get the category by id
+        return categoryDao.getById(categoryId);
+    }
+
+    // the url to return all products in category 1 would look like this
+    // https://localhost:8080/categories/1/products
+    @GetMapping("{categoryId}/products")
+    public List<Product> getProductsById(@PathVariable int categoryId)
+    {
+        // get a list of product by categoryId
+        return (List<Product>) categoryDao.getById(categoryId);
+    }
+
+    // add annotation to call this method for a POST action
+    // add annotation to ensure that only an ADMIN can call this function
+    @PostMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Category addCategory(@RequestBody Category category)
+    {
+        // insert the category
+        return categoryDao.create(category);
+    }
+@PutMapping
+@PreAuthorize("hasRole('ROLE_ADMIN')")
+    // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
+    // add annotation to ensure that only an ADMIN can call this function
+    public void updateCategory(@PathVariable int id, @RequestBody Category category)
+    {
+        categoryDao.update(id, category);
+        // update the category by id
+    }
+
+@DeleteMapping
+@PreAuthorize("hasRole('ROLE_ADMIN')")
+@ResponseStatus(value = HttpStatus.NO_CONTENT)
+    // add annotation to call this method for a DELETE action - the url path must include the categoryId
+    // add annotation to ensure that only an ADMIN can call this function
+    public void deleteCategory(@PathVariable int id)
+    {
+     deleteCategory(id);
+        // delete the category by id
+    }
+}
---
+++ src/main/java/org/yearup/data/CategoryDao.java
@@ -0,0 +1,14 @@
+package org.yearup.data;
+
+import org.yearup.models.Category;
+
+import java.util.List;
+
+public interface CategoryDao
+{
+    List<Category> getAllCategories(Integer categoryId, String name, String description);
+    Category getById(int categoryId);
+    Category create(Category category);
+    void update(int categoryId, Category category);
+    void delete(int categoryId);
+}
