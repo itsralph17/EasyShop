4af9bb29864db0f33577ff21850b15fa356d3955
itsralph17 <rapraku@appdev.yearup.org>
2023-06-27 10:56:26 -0400

    DAO ALL C

--- src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -1,17 +1,28 @@
 package org.yearup.data.mysql;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.yearup.data.CategoryDao;
 import org.yearup.models.Category;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
 {
+    private  DataSource dataSource;
+@Autowired
+    public MySqlCategoryDao(DataSource dataSource, DataSource dataSource1) {
+        super(dataSource);
+        this.dataSource = dataSource1;
+    }
+
     public MySqlCategoryDao(DataSource dataSource)
     {
         super(dataSource);
@@ -20,6 +31,36 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
     @Override
     public List<Category> getAllCategories(Integer categoryId, String name, String description)
     {
+        List<Category> categories = new ArrayList<>();
+        String sql = "SELECT * FROM categories " +
+                "WHERE (category_id = ? OR ? = -1) " +
+                "   AND (name = ? OR ? = '') " +
+                "   AND (description = ? OR ? = '') ";
+
+        categoryId = categoryId == null ? -1 : categoryId;
+        name = name == null ? "" : name;
+       description = description == null ? "" : description;
+
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, categoryId);
+            statement.setInt(2, categoryId);
+            statement.setString(3, name);
+            statement.setString(4, name);
+            statement.setString(5, description);
+            statement.setString(6, description);
+
+            ResultSet row = statement.executeQuery();
+
+            while (row.next()) {
+                Category category = mapRow(row);
+                categories.add(category);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
         // get all categories
         return null;
     }
